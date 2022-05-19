845e17091b1392499db8fbf84ccbd9f233583b3e
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-19 13:00:21 +0900

    Install @stlite/kernel and modify ConnectionManager and FileUploadClient for stlite

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2024)
+ * Copyright (c) Yuichiro Tachibana (Tsuchiya) (2022-2024)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +22,8 @@ import Hotkeys from "react-hot-keys"
 import { enableAllPlugins as enableImmerPlugins } from "immer"
 import classNames from "classnames"
 import without from "lodash/without"
+
+import { StliteKernelContext } from "@stlite/kernel"
 
 import {
   AppConfig,
@@ -245,6 +248,9 @@ export class App extends PureComponent<Props, State> {
 
   private readonly appNavigation: AppNavigation
 
+  static contextType = StliteKernelContext
+  context!: React.ContextType<typeof StliteKernelContext>
+
   public constructor(props: Props) {
     super(props)
 
@@ -414,7 +420,13 @@ export class App extends PureComponent<Props, State> {
   }
 
   initializeConnectionManager(): void {
+    const kernel = this.context?.kernel
+    if (kernel == null) {
+      throw new Error("Kernel is not set in the context.")
+    }
+
     this.connectionManager = new ConnectionManager({
+      kernel,
       sessionInfo: this.sessionInfo,
       endpoints: this.endpoints,
       onMessage: this.handleMessage,
@@ -457,6 +469,11 @@ export class App extends PureComponent<Props, State> {
   }
 
   componentDidMount(): void {
+    const kernel = this.context?.kernel
+    if (kernel == null) {
+      throw new Error("Kernel is not set in the context.")
+    }
+
     // Initialize connection manager here, to avoid
     // "Can't call setState on a component that is not yet mounted." error.
     this.initializeConnectionManager()
@@ -465,6 +482,8 @@ export class App extends PureComponent<Props, State> {
       type: "SCRIPT_RUN_STATE_CHANGED",
       scriptRunState: this.state.scriptRunState,
     })
+
+    this.uploadClient.setKernel(kernel)
 
     if (isScrollingHidden()) {
       document.body.classList.add("embedded")

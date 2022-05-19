60bb15286fe82ef53cc5a3486a364eaef2ca31a0
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-19 13:00:21 +0900

    Install @stlite/kernel and modify ConnectionManager and FileUploadClient for Stlite

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2025)
+ * Copyright (c) Yuichiro Tachibana (Tsuchiya) (2022-2024)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +24,8 @@ import { getLogger } from "loglevel"
 import moment from "moment"
 import { flushSync } from "react-dom"
 import Hotkeys from "react-hot-keys"
+
+import { StliteKernelContext } from "@stlite/kernel"
 
 import AppView from "@streamlit/app/src/components/AppView"
 import DeployButton from "@streamlit/app/src/components/DeployButton"
@@ -262,6 +265,9 @@ export class App extends PureComponent<Props, State> {
   // This will allow us to ignore finished messages from previous script runs.
   private hasReceivedNewSession: boolean = false
 
+  static override contextType = StliteKernelContext
+  override context!: React.ContextType<typeof StliteKernelContext>
+
   public constructor(props: Props) {
     super(props)
 
@@ -446,7 +452,13 @@ export class App extends PureComponent<Props, State> {
   initializeConnectionManager(): void {
     this.isInitializingConnectionManager = true
 
+    const kernel = this.context?.kernel
+    if (kernel == null) {
+      throw new Error("Kernel is not set in the context.")
+    }
+
     this.connectionManager = new ConnectionManager({
+      kernel,
       getLastSessionId: () => this.sessionInfo.last?.sessionId,
       endpoints: this.endpoints,
       onMessage: this.handleMessage,
@@ -514,6 +526,11 @@ export class App extends PureComponent<Props, State> {
   }
 
   override componentDidMount(): void {
+    const kernel = this.context?.kernel
+    if (kernel == null) {
+      throw new Error("Kernel is not set in the context.")
+    }
+
     // Initialize connection manager here, to avoid
     // "Can't call setState on a component that is not yet mounted." error.
     this.initializeConnectionManager()
@@ -523,6 +540,8 @@ export class App extends PureComponent<Props, State> {
       type: "SCRIPT_RUN_STATE_CHANGED",
       scriptRunState: this.state.scriptRunState,
     })
+
+    this.uploadClient.setKernel(kernel)
 
     if (isScrollingHidden()) {
       document.body.classList.add("embedded")

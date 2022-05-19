a314768eee34b78868af29a7b3498b33f030f603
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-19 13:00:44 +0900

    Fix ScriptRunner to use asyncio instead of threading

--- lib/streamlit/runtime/scriptrunner/script_runner.py
+++ lib/streamlit/runtime/scriptrunner/script_runner.py
@@ -14,6 +14,7 @@
 
 from __future__ import annotations
 
+import asyncio
 import gc
 import sys
 import threading
@@ -200,7 +201,8 @@ class ScriptRunner:
         self._execing = False
 
         # This is initialized in start()
-        self._script_thread: threading.Thread | None = None
+        # Stlite: threading is not supported on Pyodide, use asyncio instead
+        self._script_task: asyncio.Task | None = None
 
     def __repr__(self) -> str:
         return util.repr_(self)
@@ -234,14 +236,11 @@ class ScriptRunner:
         This must be called only once.
 
         """
-        if self._script_thread is not None:
+        # Stlite: threading is not supported on Pyodide. Use asyncio instead
+        if self._script_task is not None:
             raise Exception("ScriptRunner was already started")
 
-        self._script_thread = threading.Thread(
-            target=self._run_script_thread,
-            name="ScriptRunner.scriptThread",
-        )
-        self._script_thread.start()
+        self._script_task = asyncio.create_task(self._run_script_thread())
 
     def _get_script_run_ctx(self) -> ScriptRunContext:
         """Get the ScriptRunContext for the current thread.
@@ -269,7 +268,7 @@ class ScriptRunner:
             )
         return ctx
 
-    def _run_script_thread(self) -> None:
+    async def _run_script_thread(self) -> None:
         """The entry point for the script thread.
 
         Processes the ScriptRequestQueue, which will at least contain the RERUN
@@ -320,7 +319,8 @@ class ScriptRunner:
 
     def _is_in_script_thread(self) -> bool:
         """True if the calling function is running in the script thread"""
-        return self._script_thread == threading.current_thread()
+        # Stlite: threading is not supported on Pyodide. Use asyncio instead
+        return self._script_task == asyncio.current_task()
 
     def _enqueue_forward_msg(self, msg: ForwardMsg) -> None:
         """Enqueue a ForwardMsg to our browser queue.

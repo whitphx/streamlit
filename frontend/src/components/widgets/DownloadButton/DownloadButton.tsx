679f364d05004ceea7a24aa43f8e1c3cc6174328
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-12 14:34:41 +0900

    Update Audio, Video, ImageList, and DownloadButton to load in-memory data from stlite kernel

--- frontend/src/components/widgets/DownloadButton/DownloadButton.tsx
+++ frontend/src/components/widgets/DownloadButton/DownloadButton.tsx
@@ -25,6 +25,7 @@ import UIButton, {
 import { WidgetStateManager } from "src/lib/WidgetStateManager"
 import StreamlitMarkdown from "src/components/shared/StreamlitMarkdown"
 import { buildMediaUri } from "src/lib/UriUtil"
+import { useStliteKernel, downloadFileFromStlite } from "@stlite/kernel"
 
 export interface Props {
   disabled: boolean
@@ -38,10 +39,18 @@ function DownloadButton(props: Props): ReactElement {
   const style = { width }
   const { getBaseUriParts } = useContext(AppContext)
 
+  const stliteKernel = useStliteKernel()
+
   const handleDownloadClick: () => void = () => {
     // Downloads are only done on links, so create a hidden one and click it
     // for the user.
     widgetMgr.setTriggerValue(element, { fromUi: true })
+
+    if (element.url.startsWith("/media")) {
+      downloadFileFromStlite(stliteKernel, element.url)
+      return
+    }
+
     const link = document.createElement("a")
     const uri = `${buildMediaUri(
       element.url,

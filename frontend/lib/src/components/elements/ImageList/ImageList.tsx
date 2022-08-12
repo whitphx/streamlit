6db54446a4efcb9cf893bd5c4e7b84218c6739ac
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-12 14:34:41 +0900

    Update Audio, Video, ImageList, and DownloadButton to load in-memory data from stlite kernel

--- frontend/lib/src/components/elements/ImageList/ImageList.tsx
+++ frontend/lib/src/components/elements/ImageList/ImageList.tsx
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2026)
+ * Copyright (c) Yuichiro Tachibana (Tsuchiya) (2022-2026)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,6 +34,8 @@ import Toolbar, {
 import { useCrossOriginAttribute } from "~lib/hooks/useCrossOriginAttribute"
 import { useRequiredContext } from "~lib/hooks/useRequiredContext"
 import { StreamlitEndpoints } from "~lib/StreamlitEndpoints"
+
+import { useStliteMediaObjects } from "@stlite/kernel/react"
 
 import {
   StyledCaption,
@@ -152,6 +155,8 @@ function ImageList({
   widthConfig,
   disableFullscreenMode,
 }: Readonly<ImageListProps>): ReactElement {
+  const images = useStliteMediaObjects(element.imgs)
+
   const {
     expanded: isFullScreen,
     width,
@@ -216,7 +221,7 @@ function ImageList({
         data-testid="stImage"
         shouldStretch={shouldStretch}
       >
-        {element.imgs.map(
+        {images.map(
           (iimage, idx): ReactElement => (
             <Image
               // TODO: Update to match React best practices
--- frontend/lib/src/components/elements/Video/Video.tsx
+++ frontend/lib/src/components/elements/Video/Video.tsx
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2026)
+ * Copyright (c) Yuichiro Tachibana (Tsuchiya) (2022-2026)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +24,11 @@ import { ISubtitleTrack, Video as VideoProto } from "@streamlit/protobuf"
 import { useCrossOriginAttribute } from "~lib/hooks/useCrossOriginAttribute"
 import { StreamlitEndpoints } from "~lib/StreamlitEndpoints"
 import { WidgetStateManager as ElementStateManager } from "~lib/WidgetStateManager"
+
+import {
+  useStliteMediaObjectUrl,
+  useStliteMediaObjects,
+} from "@stlite/kernel/react"
 
 import { StyledVideoIframe } from "./styled-components"
 
@@ -48,8 +54,18 @@ function Video({
   const videoRef = useRef<HTMLVideoElement>(null)
 
   /* Element may contain "url" or "data" property. */
-  const { type, url, startTime, subtitles, endTime, loop, autoplay, muted } =
-    element
+  const {
+    type,
+    url: rawUrl,
+    startTime,
+    subtitles: rawSubtitles,
+    endTime,
+    loop,
+    autoplay,
+    muted,
+  } = element
+  const url = useStliteMediaObjectUrl(rawUrl)
+  const subtitles = useStliteMediaObjects(rawSubtitles)
 
   let crossOrigin = useCrossOriginAttribute(url)
 

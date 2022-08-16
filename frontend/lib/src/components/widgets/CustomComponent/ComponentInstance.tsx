8c755a273b2b5fe98dc827bc5d6868ef7f2ccc27
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-16 17:38:20 +0900

    Update CustomComponent for stlite

--- frontend/lib/src/components/widgets/CustomComponent/ComponentInstance.tsx
+++ frontend/lib/src/components/widgets/CustomComponent/ComponentInstance.tsx
@@ -59,8 +59,10 @@ import {
   sendRenderMessage,
 } from "./componentUtils"
 import { StyledComponentIframe } from "./styled-components"
+import { CustomComponentIFrame as StliteCustomComponentIFrame } from "@stlite/kernel/react"
 
 const LOG = getLogger("ComponentInstance")
+
 /**
  * If we haven't received a COMPONENT_READY message this many seconds
  * after the component has been created, explain to the user that there
@@ -449,7 +451,8 @@ function ComponentInstance(props: Props): ReactElement {
       {/* eslint-disable-next-line react-hooks/refs -- TODO: Do not access ref during render */}
       {loadingSkeleton}
       {warns}
-      <StyledComponentIframe
+      <StliteCustomComponentIFrame
+        IframeComponent={StyledComponentIframe}
         className="stCustomComponentV1"
         data-testid="stCustomComponentV1"
         allow={DEFAULT_IFRAME_FEATURE_POLICY}

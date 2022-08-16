64f67a0ee8c4fb887f013d2a6495bbb86944fc89
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-16 17:38:20 +0900

    Update CustomComponent for stlite

--- frontend/lib/src/components/widgets/CustomComponent/ComponentInstance.tsx
+++ frontend/lib/src/components/widgets/CustomComponent/ComponentInstance.tsx
@@ -57,6 +57,8 @@ import {
 } from "./componentUtils"
 import { StyledComponentIframe } from "./styled-components"
 
+import { CustomComponentIFrame as StliteCustomComponentIFrame } from "@stlite/kernel"
+
 /**
  * If we haven't received a COMPONENT_READY message this many seconds
  * after the component has been created, explain to the user that there
@@ -397,7 +399,8 @@ function ComponentInstance(props: Props): ReactElement {
     <>
       {loadingSkeleton}
       {warns}
-      <StyledComponentIframe
+      <StliteCustomComponentIFrame
+        IframeComponent={StyledComponentIframe}
         className="stCustomComponentV1"
         data-testid="stCustomComponentV1"
         allow={DEFAULT_IFRAME_FEATURE_POLICY}

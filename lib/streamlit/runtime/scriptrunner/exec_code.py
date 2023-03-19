c6306888938eceb399129f55a9de0d579b1caecc
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-03-19 18:18:28 +0900

    Support top-level await, adding ast.PyCF_ALLOW_TOP_LEVEL_AWAIT to the compile()'s flag and replacing exec() with await eval() to handle the coroutine obtained from compile() with the top-level await flag

--- lib/streamlit/runtime/scriptrunner/exec_code.py
+++ lib/streamlit/runtime/scriptrunner/exec_code.py
@@ -30,6 +30,7 @@ from streamlit.runtime.scriptrunner_utils.exceptions import (
 )
 
 if TYPE_CHECKING:
+    from collections.abc import Awaitable
     from types import TracebackType
 
     from streamlit.runtime.scriptrunner_utils.script_requests import RerunData
@@ -72,8 +73,8 @@ class modified_sys_path:  # noqa: N801
         return False
 
 
-def exec_func_with_error_handling(
-    func: Callable[[], Any], ctx: ScriptRunContext
+async def exec_func_with_error_handling(
+    func: Callable[[], Awaitable[Any]], ctx: ScriptRunContext
 ) -> tuple[
     Any | None,
     bool,
@@ -125,7 +126,7 @@ def exec_func_with_error_handling(
     uncaught_exception: Exception | None = None
 
     try:
-        result = func()
+        result = await func()
     except RerunException as e:
         rerun_exception_data = e.rerun_data
 

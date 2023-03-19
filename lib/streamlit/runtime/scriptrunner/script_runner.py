494b14461137108acb027172f5f5184a83d042c4
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-03-19 18:18:28 +0900

    Support top-level await, adding ast.PyCF_ALLOW_TOP_LEVEL_AWAIT to the compile()'s flag and replacing exec() with await eval() to handle the coroutine obtained from compile() with the top-level await flag

--- lib/streamlit/runtime/scriptrunner/script_runner.py
+++ lib/streamlit/runtime/scriptrunner/script_runner.py
@@ -1,4 +1,5 @@
 # Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2025)
+# Copyright (c) Yuichiro Tachibana (Tsuchiya) (2022-2025)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,8 +20,10 @@ import gc
 import sys
 import threading
 import types
+from collections.abc import Awaitable
 from contextlib import contextmanager
 from enum import Enum
+from inspect import CO_COROUTINE
 from timeit import default_timer as timer
 from typing import TYPE_CHECKING, Callable, Final, Literal, cast
 
@@ -122,7 +125,7 @@ it in the future.
 # is designed to leverage our original v1 version of multi-page apps. This
 # function will be called to run the script in lieu of the main script. This
 # function simulates the v1 setup using the modern v2 commands (st.navigation)
-def _mpa_v1(main_script_path: str):
+async def _mpa_v1(main_script_path: str):
     from pathlib import Path
 
     from streamlit.commands.navigation import PageType, _navigation
@@ -157,7 +160,9 @@ def _mpa_v1(main_script_path: str):
 
     if page._page != main_page._page:
         # Only run the page if it is not pointing to this script:
-        page.run()
+        maybe_awaitable = page.run()
+        if isinstance(maybe_awaitable, Awaitable):
+            await maybe_awaitable
         # Finish the script execution here to only run the selected page
         raise StopException()
 
@@ -364,7 +369,7 @@ class ScriptRunner:
             # request that we'll handle immediately. When the script finishes,
             # it's possible that another request has come in that we need to
             # handle, which is why we call _run_script in a loop.
-            self._run_script(request.rerun_data)
+            await self._run_script(request.rerun_data)
             request = self._requests.on_scriptrunner_ready()
 
         assert request.type == ScriptRequestType.STOP
@@ -448,7 +453,7 @@ class ScriptRunner:
         finally:
             self._execing = False
 
-    def _run_script(self, rerun_data: RerunData) -> None:
+    async def _run_script(self, rerun_data: RerunData) -> None:
         """Run our script.
 
         Parameters
@@ -581,7 +586,7 @@ class ScriptRunner:
             # assume is the main script directory.
             module.__dict__["__file__"] = script_path
 
-            def code_to_exec(code=code, module=module, ctx=ctx, rerun_data=rerun_data):
+            async def code_to_exec(code=code, module=module, ctx=ctx, rerun_data=rerun_data):
                 with (
                     modified_sys_path(self._main_script_path),
                     self._set_execing_flag(),
@@ -636,8 +641,12 @@ class ScriptRunner:
 
                     else:
                         if PagesManager.uses_pages_directory:
-                            _mpa_v1(self._main_script_path)
-                        exec(code, module.__dict__)
+                            await _mpa_v1(self._main_script_path)
+                        if code.co_flags & CO_COROUTINE:
+                            # The source code includes top-level awaits, so the compiled code object is a coroutine.
+                            await eval(code, module.__dict__)
+                        else:
+                            exec(code, module.__dict__)
                         self._fragment_storage.clear(
                             new_fragment_ids=ctx.new_fragment_ids
                         )
@@ -653,7 +662,7 @@ class ScriptRunner:
                 rerun_exception_data,
                 premature_stop,
                 uncaught_exception,
-            ) = exec_func_with_error_handling(code_to_exec, ctx)
+            ) = await exec_func_with_error_handling(code_to_exec, ctx)
             # setting the session state here triggers a yield-callback call
             # which reads self._requests and checks for rerun data
             self._session_state[SCRIPT_RUN_WITHOUT_ERRORS_KEY] = run_without_errors

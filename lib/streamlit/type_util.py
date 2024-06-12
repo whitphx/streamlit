1faf1f5c54419cbddf6fd38f40e5f5e9814a66fe
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-06-12 06:49:30 +0100

    Make st.write_stream() async to support async generator and hack the script compiler to convert function calls of st.write_stream() to be awaited under the hood (#11, #15, #17)

--- lib/streamlit/type_util.py
+++ lib/streamlit/type_util.py
@@ -1,4 +1,5 @@
 # Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2025)
+# Copyright (c) Yuichiro Tachibana (Tsuchiya) (2022-2024)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -460,3 +461,9 @@ def async_generator_to_sync(
         pass
     finally:
         loop.close()
+
+
+async def generator_to_async(gen: Generator[Any, Any]) -> AsyncGenerator[Any, Any, Any]:
+    """Convert a generator to an async generator."""
+    for item in gen:
+        yield item
